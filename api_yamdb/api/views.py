c000f0de050aeb5d29bcf714935b008ffcf65437
Виктор Рыженко <victor-ozr@yandex.ru>
2023-05-09 19:33:43 +0500

    Создана модель и api по Categories

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.pagination import LimitOffsetPagination
+from reviews.models import Category
+from .serializers import CategorySerializer
 
-# Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAdminUser]
+    pagination_class = LimitOffsetPagination
