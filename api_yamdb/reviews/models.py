ba9cd1d7f9cae48a7a0c4efeff1f95a8d0afea66
Виктор Рыженко <victor-ozr@yandex.ru>
2023-05-11 00:15:08 +0500

    Подготовлены модели, view и эндпойнты для жанров

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -11,3 +11,15 @@ class Category(models.Model):
         unique=True,
         max_length=50)
     description = models.TextField()
+
+
+class Genre(models.Model):
+    """Категории жанров"""
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название жанра',
+        help_text='Выбрать жанр')
+    slug = models.SlugField(
+        unique=True,
+        max_length=50)
+    description = models.TextField()
