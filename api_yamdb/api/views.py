5328504f47b6e3aa1154cd16ecee4f500ea11ad3
Виктор Рыженко <victor-ozr@yandex.ru>
2023-05-11 21:58:36 +0500

    Исправление пермишенов по категориям и по жанрам

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,18 +1,37 @@
-from rest_framework import viewsets, permissions
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
 from rest_framework.pagination import LimitOffsetPagination
-from reviews.models import Category, Genre
-from .serializers import CategorySerializer, GenreSerializer
+from reviews.models import Category, Review, Genre
+from .serializers import CategorySerializer, CommentSerializer, GenreSerializer
+from .permissions import AdminOrAuthorOrReadOnly, AdminOrReadOnly
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [AdminOrReadOnly]
     pagination_class = LimitOffsetPagination
 
 
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [
+        AdminOrAuthorOrReadOnly
+    ]
+    pagination_class = LimitOffsetPagination
+
+    def get_review(self):
+        return get_object_or_404(Review, pk=self.kwargs.get('review_id'))
+
+    def get_queryset(self):
+        return self.get_review().comments.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, review=self.get_review())
+
+        
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [AdminOrReadOnly]
     pagination_class = LimitOffsetPagination
