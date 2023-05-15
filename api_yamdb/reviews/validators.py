98e1091c8bcb325958d7ec360127e55dd0e6edca
Виктор Рыженко <victor-ozr@yandex.ru>
2023-05-15 22:04:59 +0500

    Исправлены валидаторы у моделей rewiew

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -7,15 +7,13 @@ def get_current_year():
     return datetime.now().year
 
 
-def get_max_year_for_title():
-    return MaxValueValidator(
+get_max_year_for_title = MaxValueValidator(
         limit_value=get_current_year,
         message='Произведение еще не вышло!'
     )
 
 
-def slug_validator():
-    return RegexValidator(
+slug_validator = RegexValidator(
         regex='^[-a-zA-Z0-9_]+$',
         message=(
             R'Возможны только символы латинского алфавита,'
